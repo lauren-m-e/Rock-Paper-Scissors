71c14a736e34bd3019cd71c5139d7576a48ac590
Lauren <lauren.m.e.l@proton.me>
2023-04-24 02:32:12 -0400

    Generated Computer's Hand

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,3 +1,18 @@
-function getComputerChoice () {
-    getRandomInt(3)};
-console.log(getComputerChoice);
+function getcomputerSelection (max) {
+    return Math.floor(Math.random() * max);
+}
+
+var numberINT = getcomputerSelection(3);
+console.log(numberINT);
+
+if (numberINT === 0) {
+    var computerSelection = "Rock";
+} else if (numberINT === 1) {
+    var computerSelection = "Paper";
+} else {
+    var computerSelection = "Scissors";
+}
+console.log("Computer Selection is " + computerSelection);
+
+
+
