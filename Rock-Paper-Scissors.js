b07c9308d4fb69a0d2a920cc6b1f63a025802a23
Lauren <lauren.m.e.l@proton.me>
2023-04-26 01:40:38 -0400

    Plays 5 times

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,18 +1,62 @@
-function getcomputerSelection (max) {
-    return Math.floor(Math.random() * max);
+function getComputerChoice(numberINT, cSelect){
+    var numberINT = Math.floor(Math.random()*3);
+    console.log("var numberINT = " + numberINT);
+    
+    if (numberINT === 0) {
+        var cSelect = "rock";
+    } else if (numberINT === 1) {
+        var cSelect = "paper";
+    } else {
+        var cSelect = "scissors";
+    }
+    console.log("var cSelect = " + cSelect);
+    return cSelect
+}
+console.log("function getComputerChoice = " + getComputerChoice())
+
+function playRound(pSelect, cSelect) {
+var pSelect = prompt("pick one").toLowerCase();
+console.log("var pSelect = " + pSelect)
+var cSelect = getComputerChoice()
+console.log("var cSelect = " + cSelect)
+if (cSelect===pSelect){
+    return "tie"
 }
 
-var numberINT = getcomputerSelection(3);
-console.log(numberINT);
-
-if (numberINT === 0) {
-    var computerSelection = "Rock";
-} else if (numberINT === 1) {
-    var computerSelection = "Paper";
-} else {
-    var computerSelection = "Scissors";
+else if (cSelect === "rock") {
+    if (pSelect==="paper") {
+        return "WINNNN!!! " + pSelect+" beats "+cSelect+"!"
+    }
+    else {
+        return "Lose!!! "+cSelect+" beats "+pSelect+"!"
+    }
 }
-console.log("Computer Selection is " + computerSelection);
+else if (cSelect==="paper") {
+    if (pSelect==="scissors") {
+        return "WINNNN!!! " + pSelect+" beats "+cSelect+"!!"
+    }
+    else {
+        return "Lose!!! "+cSelect+" beats "+pSelect+"!"
+    }
+}
+else if (cSelect==="scissors") {
+    if (pSelect==="rock") {
+        return "WINNNN!!! " + pSelect+" beats "+cSelect+"!!!"
+    }
+    else {
+        return "Lose!!! "+cSelect+" beats "+pSelect+"!"
+    }
+}
+}
 
 
+console.log(playRound());
+
+console.log(playRound());
+
+console.log(playRound());
+
+console.log(playRound());
+
+console.log(playRound());
 
