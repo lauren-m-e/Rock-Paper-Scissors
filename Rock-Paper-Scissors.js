c5908bed78d4d716ee308240f6ed0a7f669c45a5
Lauren <lauren.m.e.l@proton.me>
2023-05-07 23:33:52 -0400

    Cleaned code

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -2,7 +2,7 @@ let cScore = 0
 let pScore = 0
 function getComputerChoice(numberINT, cSelect){
     var numberINT = Math.floor(Math.random()*3);
-    console.log("var numberINT = " + numberINT);
+    //console.log("var numberINT = " + numberINT);
     
     if (numberINT === 0) {
         var cSelect = "rock";
@@ -11,50 +11,46 @@ function getComputerChoice(numberINT, cSelect){
     } else {
         var cSelect = "scissors";
     }
-    console.log("var cSelect = " + cSelect);
+    //console.log("var cSelect = " + cSelect);
     return cSelect
 }
-console.log("function getComputerChoice = " + getComputerChoice())
+//console.log("function getComputerChoice = " + getComputerChoice())
 
 function playRound(pSelect, cSelect) {
 var pSelect = prompt("pick one").toLowerCase();
-console.log("var pSelect = " + pSelect)
+//console.log("var pSelect = " + pSelect)
 var cSelect = getComputerChoice()
-console.log("var cSelect = " + cSelect)
+//.log("var cSelect = " + cSelect)
 if (cSelect===pSelect){
-    return "tie! Player Score = " + pScore + "; Computer Score = " + cScore;
+    return "tie! \nPlayer Score = " + pScore + "\n Computer Score = " + cScore;
 } else if (cSelect === "rock") {
     if (pSelect==="paper") {
-        return "WINNNN!!! " + pSelect+" beats "+cSelect+"! Player Score = " + ++pScore + "; Computer Score = " + cScore;
+        return "WINNNN!!! " + pSelect+" beats "+cSelect+"! \nPlayer Score = " + ++pScore + "\nComputer Score = " + cScore;
     }
     else {
-        return "Lose!!! "+cSelect+" beats "+pSelect+"! Player Score = " + pScore + "; Computer Score = " + ++cScore;
+        return "Lose!!! "+cSelect+" beats "+pSelect+"! \nPlayer Score = " + pScore + "\nComputer Score = " + ++cScore;
     }
 } else if (cSelect==="paper") {
     if (pSelect==="scissors") {
-        return "WINNNN!!! " + pSelect+" beats "+cSelect+"! Player Score = " + ++pScore + "; Computer Score = " + cScore;
+        return "WINNNN!!! " + pSelect+" beats "+cSelect+"! \nPlayer Score = " + ++pScore + "\nComputer Score = " + cScore;
     }
     else {
-        return "Lose!!! "+cSelect+" beats "+pSelect+"! Player Score = " + pScore + "; Computer Score = " + ++cScore;
+        return "Lose!!! "+cSelect+" beats "+pSelect+"! \nPlayer Score = " + pScore + "\nComputer Score = " + ++cScore;
     }
 } else if (cSelect==="scissors") {
     if (pSelect==="rock") {
-        return "WINNNN!!! " + pSelect+" beats "+cSelect+"! Player Score = " + ++pScore + "; Computer Score = " + cScore;
-    }s
+        return "WINNNN!!! " + pSelect+" beats "+cSelect+"\nPlayer Score = " + ++pScore + "\nComputer Score = " + cScore;
+    }
     else {
-        return "Lose!!! "+cSelect+" beats "+pSelect+"! Player Score = " + pScore + "; Computer Score = " + ++cScore;
+        return "Lose!!! "+cSelect+" beats "+pSelect+"\nPlayer Score = " + pScore + "\nComputer Score = " + ++cScore;
     }
 }
 }
 
 
 console.log(playRound());
-console.log(cScore);
 console.log(playRound());
-console.log(cScore);
 console.log(playRound());
-console.log(cScore);
 console.log(playRound());
-console.log(cScore);
 console.log(playRound());
-console.log(cScore);
+
